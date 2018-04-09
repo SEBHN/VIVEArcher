187dbd8f394a8088fbe3ec14d2d506edc944e67b
Unknown <cuong_47@yahoo.de>
2018-04-09 16:52:16 +0200

    Added virtual button to the world space Clicking the button change its color randomly

---
+++ Assets/Scripts/ChangeButtonColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButtonColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeColorOfButton(Renderer renderer)
+    {
+        renderer.material.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
+    }
+}
