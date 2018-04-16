35a3ca7503612734919d70c89be4eba9398bde01
Unknown <cuong_47@yahoo.de>
2018-04-16 16:09:35 +0200

    remove useless stuff

--- Assets/Scripts/HandRayCast.cs
+++ Assets/Scripts/HandRayCast.cs
@@ -14,15 +14,7 @@ public class HandRayCast : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        /*Controller controller = new Controller();
-        Frame frame = controller.Frame(); // controller is a Controller object
-        if (frame.Hands.Count > 0)
-        {
-            List<Hand> hands = frame.Hands;
-            leapHand = hands[0];
-        }
-        finger = leapHand.Fingers;
-        Finger.FingerType fingerType = finger.Type;*/
+
         leftInteractionHand = GetComponent<InteractionHand>();
         leapHand = leftInteractionHand.leapHand;
         if (leapHand == null) Debug.LogError("No leap_hand founded");
@@ -32,14 +24,11 @@ public class HandRayCast : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        //for (int i = 0; i < HandModel.NUM_FINGERS; i++)
-        //{
-            finger = leapHand.Fingers[1]; // just index finger
-            if (Physics.Raycast(finger.TipPosition.ToVector3(), finger.Direction.ToVector3(), out hit, Mathf.Infinity) && hit.collider.tag == "Respawn")
-            {
-                hit.collider.GetComponent<Renderer>().material.color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
-            }
-            Debug.DrawRay(finger.TipPosition.ToVector3(), finger.Direction.ToVector3(), Color.red, Time.deltaTime, true);
-        //}
+        finger = leapHand.Fingers[1]; // just index finger
+        if (Physics.Raycast(finger.TipPosition.ToVector3(), finger.Direction.ToVector3(), out hit, Mathf.Infinity) && hit.collider.tag == "Respawn")
+        {
+            hit.collider.GetComponent<Renderer>().material.color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+        }
+        Debug.DrawRay(finger.TipPosition.ToVector3(), finger.Direction.ToVector3(), Color.red, Time.deltaTime, true);
     }
 }
