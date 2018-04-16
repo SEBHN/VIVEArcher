713611acdb4a78d2c4319f911c217961aa60e3ef
Manuel <wtfjoke@livenet.ch>
2018-04-16 16:42:19 +0200

    enable bow menu (buggy, though)

---
+++ Assets/DisableEnableBowMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableEnableBowMenu : MonoBehaviour {
+
+    public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.rotation.eulerAngles.z > 90)
+        {
+            menu.SetActive(true);
+        }
+        else
+        {
+            menu.SetActive(false);
+        }
+	}
+}
