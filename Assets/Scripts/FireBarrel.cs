c20daa773d88eb43c5b5667d968f209a8a3f0da8
Manuel <wtfjoke@livenet.ch>
2018-04-30 16:03:51 +0200

    add firebarrel

---
+++ Assets/Scripts/FireBarrel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBarrel : MonoBehaviour
+{
+
+    public GameObject fireArePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "projectile")
+        {
+            GameObject fireFloor = Instantiate(fireArePrefab, transform.position - new Vector3(0, transform.position.y, 0) + new Vector3(0, fireArePrefab.transform.position.y, 0), Quaternion.identity);
+            Destroy(fireFloor, 10f);
+            Destroy(gameObject);
+        }
+    }
+}
